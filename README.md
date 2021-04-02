c5cf8a5e21552c1d4bb67c5eb1d68018bf515841
nylas-bohao <bohao.z@nylas.com>
2021-04-02 18:13:55 -0400

    upload the code

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # webhook-cb-page
+Create a webhook callback site to catch the webhook payloads
+
+# Set up Environment
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
+# Run App
+python app.py
+App will listen on localhost:5000
+
+# Make the App public
+You will need to download/register ngrok, find download link here https://ngrok.com/
+Follow the instruction once you log in, once your ngrok is ready, run:
+./ngrok http 5000
+Copy the ngrok HTTPS URL to your Dashboard/Webhooks as the Callback URL, you will now receive webhook payloads!
---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+import pprint
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def print_test():
+
+    if request.method == 'POST':
+        request_data = request.get_json()
+        print('=================Webhook Payload=================')
+        pprint.pprint(request_data)
+        print('=================================================')
+        return 'OK'
+
+    query_parameters = request.args
+    if request.method == 'GET':
+        challenge = query_parameters.get('challenge')
+        if challenge != None:
+            return challenge
+        else:
+            return "I'm a page"
+
+if __name__ == '__main__':
+    app.run(debug=True)
